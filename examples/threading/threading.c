9e81358457b48aa8b71f1b5b83355886784d079c
Finn Hering <finn@hering.sh>
2026-08-04 12:37:18 +0200

    assignment part 4 submission

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,7 +1,10 @@
+#define _GNU_SOURCE // use the gnu extension so we have pthread_tryjoin_mp available
 #include "threading.h"
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -11,9 +14,34 @@
 void* threadfunc(void* thread_param)
 {
 
+    int err = 0;
+    struct thread_data* td = thread_param;
+    DEBUG_LOG("sleeping for: %dms", td->wait_to_obtain_ms);
+    usleep(td->wait_to_obtain_ms * 1000);
+    DEBUG_LOG("Done sleeping...");
+    if((err = pthread_mutex_lock(td->mutex))) {
+        ERROR_LOG("Unable to obtain mutex: %s", strerror(err));
+        td->thread_complete_success = false;
+        return thread_param;
+    } else {
+        DEBUG_LOG("Lock acquired!");
+    }
+    DEBUG_LOG("sleeping for: %dms", td->wait_to_obtain_ms);
+    usleep(td->wait_to_release_ms * 1000);
+    DEBUG_LOG("Done sleeping...");
+
+    if((err = pthread_mutex_unlock(td->mutex))) {
+        ERROR_LOG("Unable to unlock mutex: %s", strerror(err));
+        td->thread_complete_success = false;
+        return thread_param;
+    } else {
+        DEBUG_LOG("Unlocked mutex!");
+    }
+
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    td->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +56,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data* td = malloc(sizeof(struct thread_data));
+    int err = 0;
+    td->mutex = mutex;
+    td->wait_to_obtain_ms = wait_to_obtain_ms;
+    td->wait_to_release_ms = wait_to_release_ms;
+
+    if((err = pthread_create(thread, NULL, threadfunc, td))) {
+        ERROR_LOG("Unable to start thread: %s", strerror(err));
+        return false;
+    } else {
+        DEBUG_LOG("Thread created successful!");
+    }
+
+    return true;
 }
 
